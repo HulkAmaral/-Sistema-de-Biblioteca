6d4676e6192d80f6cd8ecf2f3cd22be349489a8f
HulkAmaral <joao140397@gmail.com>
2025-09-25 19:50:34 -0300

    Update JwtUtil.java

--- src/config/JwtUtil.java
+++ src/config/JwtUtil.java
@@ -1,1 +1,31 @@
+package com.example.pm.config;
 
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+
+@Component
+public class JwtUtil {
+@Value("${jwt.secret}")
+private String secret;
+@Value("${jwt.expiration-ms}")
+private long expirationMs;
+
+
+public String generateToken(String username){
+return Jwts.builder()
+.setSubject(username)
+.setIssuedAt(new Date())
+.setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+.signWith(SignatureAlgorithm.HS256, secret)
+.compact();
+}
+
+
+public String extractUsername(String token){
+return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+}
+}
