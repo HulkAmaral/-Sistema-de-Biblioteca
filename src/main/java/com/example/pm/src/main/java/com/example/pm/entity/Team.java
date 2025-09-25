99c43af4a4f7f04dd461f9dd29275d378ae1557a
HulkAmaral <joao140397@gmail.com>
2025-09-25 19:28:39 -0300

    Update Team.java

--- src/main/java/com/example/pm/src/main/java/com/example/pm/entity/Team.java
+++ src/main/java/com/example/pm/src/main/java/com/example/pm/entity/Team.java
@@ -1,1 +1,27 @@
+package com.example.pm.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "teams")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Team {
+@Id
+@GeneratedValue(strategy = GenerationType.UUID)
+private String id;
+
+
+private String name;
+private String description;
+
+
+@ManyToMany
+@JoinTable(name = "team_user",
+joinColumns = @JoinColumn(name = "team_id"),
+inverseJoinColumns = @JoinColumn(name = "user_id"))
+private Set<User> members;
+}
