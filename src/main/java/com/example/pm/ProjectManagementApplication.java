dbcbfd6ee71d609958172b740cdcb4d5467ffd83
HulkAmaral <joao140397@gmail.com>
2025-09-25 19:22:05 -0300

    Update ProjectManagementApplication.java

--- src/main/java/com/example/pm/ProjectManagementApplication.java
+++ src/main/java/com/example/pm/ProjectManagementApplication.java
@@ -1,1 +1,13 @@
-public class ProjectManagementApplication {}
+package com.example.pm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+public static void main(String[] args) {
+SpringApplication.run(ProjectManagementApplication.class, args);
+}
+}
