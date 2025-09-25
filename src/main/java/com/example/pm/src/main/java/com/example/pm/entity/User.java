552f7cb33f49ce2ff869affa05ee1696b0ec19b6
HulkAmaral <joao140397@gmail.com>
2025-09-25 19:27:24 -0300

    Update User.java

--- src/main/java/com/example/pm/src/main/java/com/example/pm/entity/User.java
+++ src/main/java/com/example/pm/src/main/java/com/example/pm/entity/User.java
@@ -1,1 +1,35 @@
+package com.example.pm.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.example.pm.entity.enums.Role;
+
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+@Id
+@GeneratedValue(strategy = GenerationType.UUID)
+private String id;
+
+
+private String fullName;
+@Column(unique = true, length = 11)
+private String cpf;
+@Column(unique = true)
+private String email;
+
+
+private String position; // cargo
+
+
+@Column(unique = true)
+private String username;
+private String password;
+
+
+@Enumerated(EnumType.STRING)
+private Role role;
+}
