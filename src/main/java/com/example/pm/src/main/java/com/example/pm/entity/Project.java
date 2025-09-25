55a541fac0a7ad8c960261e2b90b284aa2407247
HulkAmaral <joao140397@gmail.com>
2025-09-25 19:28:10 -0300

    Update Project.java

--- src/main/java/com/example/pm/src/main/java/com/example/pm/entity/Project.java
+++ src/main/java/com/example/pm/src/main/java/com/example/pm/entity/Project.java
@@ -1,1 +1,40 @@
+package com.example.pm.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.example.pm.entity.enums.ProjectStatus;
+import java.time.LocalDate;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "projects")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Project {
+@Id
+@GeneratedValue(strategy = GenerationType.UUID)
+private String id;
+
+
+private String name;
+private String description;
+private LocalDate startDate;
+private LocalDate endDate;
+
+
+@Enumerated(EnumType.STRING)
+private ProjectStatus status;
+
+
+@ManyToOne
+@JoinColumn(name = "manager_id")
+private User manager;
+
+
+@ManyToMany
+@JoinTable(name = "project_team",
+joinColumns = @JoinColumn(name = "project_id"),
+inverseJoinColumns = @JoinColumn(name = "team_id"))
+private Set<Team> teams;
+}
